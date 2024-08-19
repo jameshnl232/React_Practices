8ad9994b2243c9e36550aa58819c7f6f8dee525d
luong <luongpower232@gmail.com>
2024-08-19 15:11:03 +0200

    add base

--- food-recipe-app/vite.config.js
+++ food-recipe-app/vite.config.js
@@ -4,5 +4,5 @@ import react from '@vitejs/plugin-react'
 // https://vitejs.dev/config/
 export default defineConfig({
   plugins: [react()],
-  base: "/food-recipe-app",
+  base: "/food-recipe-app/",
 });
